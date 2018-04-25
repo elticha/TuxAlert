44e29cd5d286f946f55466a55335264621eb5886
Fernando <163189@ids.upchiapas.edu.mx>
2018-04-25 15:32:48 -0500

    Carpeta del proyecto creada
    Go ahead

---
+++ App/app/src/main/java/lambdastudios/tuxalert/tarjetaBienvenida1.java
@@ -0,0 +1,13 @@
+package lambdastudios.tuxalert;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class tarjetaBienvenida1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tarjeta_bienvenida1);
+    }
+}
